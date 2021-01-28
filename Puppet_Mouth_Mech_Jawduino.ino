a699b753f409d4a0db589bc92ca85093adfc5281
Derrick Bommarito <derrick.a.bommarito@gmail.com>
2021-01-28 01:04:29 -0600

    Mostly updated?
    Need to add in random neck movement and probably polish a bunch of shit up.

--- Puppet_Mouth_Mech_Jawduino.ino
+++ Puppet_Mouth_Mech_Jawduino.ino
@@ -3,105 +3,322 @@
 to make a servo move in sync with audio.
 *******************/
 #include <Servo.h>
-#include "SoftRcPulseOut.h"
+#include <math.h>
 
-Servo rotation;
+#define MOUTH_CLOSED 0
+#define MOUTH_OPEN 60
 
+#define NECK_LEFT 60
+#define NECK_CENTER 90
+#define NECK_RIGHT 120
 
-#define TEST_PIN 11 //pin 11 set to ground will kick off the servo sweep test
-int audio_value = 0;
-long lastMsg = 0;
-long sleepWindow = 300000; //if 5 minutes go by with no signal, then put the servos to bed
-SoftRcPulseOut servo;
-volatile boolean servosEnabled = false;
-volatile boolean ledsOn = true;
-volatile unsigned long currentTime = 0;
-volatile unsigned long lastLEDtime = 0;
-unsigned long resetWait = 120000; //servos sleep if not changed within this time frame (120 secs)
+#define PIN_MOUTH 2
+#define PIN_NECK 4
 
+#define ADC_COUNT 3 // number of ADC channels to read for position (starting at A0)
 
+#define JAW_LIMIT 86 //should be 86 or 87 to match other value
 
-void setup()
-{
+#define SERVO_TIMEOUT_MINS 3 //sleep servos after 5 minutes of no sound or input
 
-  Serial.begin(115200);
-  set_minmax();
-  pinMode(TEST_PIN,INPUT);  //pin 11 will be 
-  digitalWrite(TEST_PIN,HIGH); //assign pull-up resistor
-} 
+//Specifies the number of seconds automated control of features will be paused
+//after receiving manual control for something.
+//IE After sending a neck angle, it will wait this long before picking a new
+//angle to look towards.
+#define MANUAL_PAUSE_SECS 10
 
-void loop()
-{
-  servo_test();
-  audio_updates(); //read sample from audio input and apply average to buffer
-  if(servosEnabled) 
-  {
-  action(); //servos are handled in the action loop
-  SoftRcPulseOut::refresh();
-  if((millis() - lastMsg) > sleepWindow)
-    detach_servos();
+//================================================
+// This should be the end of the tweakable values
+//================================================
+
+#define SLEEP_MS (SERVO_TIMEOUT_MINS * 1000 * 60) //if specified minutes go by with no signal, then put the servos to bed
+#define PAUSE_MS (MANUAL_PAUSE_SECS * 1000)
+
+boolean servosEnabled = false;
+
+Servo servoMouth;
+Servo servoNeck;
+
+//Stores the current ADC channel being read
+volatile uint8_t curADC = 0;
+
+//Stores the value of the jaw as the channels are being read
+volatile uint8_t nextJawValue = 0;
+
+//REFS1:0 sets to the AVCC voltage ref
+  
+//ADLAR left aligns the ADC value into the high byte
+//instead of only the upper 2 bits of the 10-bit
+//ADC value being in the high byte.
+//This lets us just read an 8bit value
+const uint8_t admuxDefault = (0 << REFS1) | (1 << REFS0) | (1 << ADLAR);
+
+//This gets set everytime we finish the round of analog inputs
+//means there's a new valid value to read from jawValue
+volatile bool cycleComplete = false;
+
+volatile uint8_t jawValue = 0;
+
+volatile bool runAnalogs = false;
+
+//stores the current millis counter each loop iteration
+//this lets the entire loop iteration run at the "same" time
+//and we don't have to call millis multiple times
+unsigned long curTime;
+
+//last* stores the last time that feature was processed
+unsigned long lastMouth;
+unsigned long lastNeck;
+
+//period* is time in ms between updates of that feature
+unsigned long periodMouth = 20;
+unsigned long periodNeck = 50;
+
+//For head turning we want to enable speed control and
+//we want to slowly move towards a target over several
+//update iterations.
+//Floats are used for smoother processing across iterations
+//but will be casted to ints when updating the servos.
+float neckPos = NECK_CENTER;
+float neckSpeed = 40.0; // in degrees per second
+float neckTarget = NECK_CENTER;
+float neckDT = periodNeck / 1000.0;
+
+//Mouth is an instantaneous setting based on the
+//audio level so we don't need to do fancy speed
+//control or anything. Yet. We can add some
+//other features like interpolation in the future.
+float curMouth = MOUTH_CLOSED;
+
+//Flag for whether automatic control of mouth (audio level)
+//is enabled or not. Can disable auto via serial commands
+//if wanting full manual control or to pause mouth action.
+bool autoMouth = true;
+
+//Flag to enable random gazing around
+bool autoNeck = true;
+
+//lastManual* tracks last manual command for feature so auto
+//can be paused for the specified duration
+unsigned long lastManualMouth = 0;
+unsigned long lastManualNeck = 0;
+
+//This is automagically called (when enabled) as soon as ADC finishes a conversion
+ISR(ADC_vect) {
+  //Read just the upper 8 bits (of the 10) of the ADC value
+  uint8_t val = ADCH;
+
+  //check threshold
+  if (val < JAW_LIMIT) {
+    ++nextJawValue;
   }
-} 
+
+  //increment ADC # and check if we've finished
+  if (++curADC == ADC_COUNT) {
+    curADC = 0;
+    jawValue = nextJawValue;
+    nextJawValue = 0;
+    cycleComplete = true;
+  }
+
+  //Set the input to the next pin to read
+  ADMUX = admuxDefault | curADC;
+}
+
+void initAnalog() {
+  /* Going to do some specific setup to minimize the number of registers
+   *  we have to touch during reads since we don't need much resolution
+   *  we're just looking for a threshold (347 -> 86/87)
+   */
+  ADMUX = admuxDefault;
+
+  //ADEN -> Enable ADC
+  
+  //ADIE -> Enable ADC Interrupt
+  // We use the interrupt to constantly cycle through the inputs
+  
+  //ADPS# -> Prescaler values that drops 16MHz clock down to 125kHz - divides by 128
+  //Conversions take 13 ADC clock cycles so this means about 9600 samples per sec
+  // and with 3 channels this is roughly 3kHz that can be run which is WAY
+  // faster than the 50Hz servos are typically updated at which means
+  // we can add in better processing in the future if we want fancier response.
+  ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
+
+void startAnalog() {
+  runAnalogs = true;
+  
+  //set the start conversion bit in the status and control register
+  ADCSRA |= (1 << ADSC);
+}
+
+void stopAnalog() {
+  //signal other analog functions that analog is not running
+  runAnalogs = false;
+
+  //disable ADC complete interrupt
+  ADCSRA &= ~(1 << ADIE);
+}
+
 void attach_servos()
 {   // attach the pin to the servo object
-    servo.attach(2); // pin 2 for mouth
-    rotation.attach(4); // Pin 4 for Head swivel servo
+    servoMouth.attach(PIN_MOUTH); // pin 2 for mouth
+    servoNeck.attach(PIN_NECK); // Pin 4 for Head swivel servo
     servosEnabled = true;
 }
 void detach_servos()
 {   // detach the servo objects
-    servo.detach();
+    servoMouth.detach();
+    servoNeck.detach();
     servosEnabled = false;
 }
-void servo_test()
-{
-  if(digitalRead(TEST_PIN) == HIGH) return;
-  attach_servos();
-  SoftRcPulseOut::refresh();
-  for(int i = 0; i < 360;i++)
-  {
-    if(i < 180)
-      audio_value = i;
-    else
-      audio_value = 359 - i;
-    action();
-    for(int i = 0; i < 10; i++)
-    {
-       delay(1); 
-       SoftRcPulseOut::refresh();
+
+void updateMouth() {
+  //check if we're outside a pause window and return to auto control
+  if (autoMouth && curTime - lastManualMouth > PAUSE_MS) {
+    if (cycleComplete) {
+      //make a local copy of the value from the interrupt to prevent "issues"
+      uint8_t jaw = jawValue;
+
+      //clear the flag so we can wait for a new audio value.
+      //we might actually miss a few but since the servo can only move
+      //so fast it's not an issue. This flag will be more useful if
+      //trying more processing on jaw movement outside of servo updates.
+      cycleComplete = false;
+
+      float jawRatio = jaw / (ADC_COUNT - 1);
+      curMouth = ((MOUTH_OPEN - MOUTH_CLOSED) * jawRatio) + MOUTH_CLOSED;
+
+      servoMouth.write(curMouth);
     }
   }
-  detach_servos();
 }
-void audio_updates()
-{
-  audio_value = 0;
-  if(analogRead(A0) < 341) audio_value += 60;
-  if(analogRead(A1) < 341) audio_value += 60;
-  if(analogRead(A2) < 341) audio_value += 60;
 
-  if(audio_value > 0) 
-  {
-    lastMsg = millis(); //save the time stamp from when we last had some action
-    if(!servosEnabled)attach_servos();
+void updateNeck() {
+  if (autoNeck && curTime - lastManualNeck > PAUSE_MS) {
+    //anytime we're within 1 degree of where we want to be, just count as there
+    float neckDiff = neckTarget - neckPos;
+    
+    if (fabs(neckDiff) > 1.0) {
+      if (neckDiff < 0.0) { // to the left
+        neckPos -= neckSpeed * neckDT;
+        if (neckPos < neckTarget) neckPos = neckTarget;
+      } else { //to the right
+        neckPos += neckSpeed * neckDT;
+        if (neckPos > neckTarget) neckPos = neckTarget;
+      }
+    } else {
+      neckPos = neckTarget;
+    }
   }
+
+  servoNeck.write(neckPos);
 }
-void action() { 
-  if (!servosEnabled) attach_servos();
-  // set Neck servo back to 90 degress when audio comes in
-  rotation.write(90);
-  delay(15);
-  servo.write(audio_value);
-  SoftRcPulseOut::refresh();
- } 
-void set_minmax()
+
+void setup()
 {
-  //set the first parameter in the following functions to a number between 0 and 180.
-  //I used 92 and 72 in my tests to give about 20 degrees of motion.
-  //You may swap the large and small numbers to reverse direction.
-  //Just play with them, upload the code, then ground pin 11 to run the sweep test.
-  // Be sure to only play with these numbers while the jaw linkage is disconnected,
-  //  otherwise, you risk hitting mechanical limits and damaging your linkage or servo!
-  servo.setMinimumPulse(map(0,0,180,512,2400));
-  servo.setMaximumPulse(map(60,0,180,512,2400));
-}
+  //initialize serial first so we can use it for debugging output
+  Serial.begin(115200);
+
+  initAnalog();
+  attach_servos();
+
+  startAnalog();
+
+  curTime = millis();
+  lastMouth = curTime;
+  lastNeck = curTime;
+} 
+
+void loop()
+{
+  curTime = millis();
+
+  //start checking update timing for features.
+
+  //mouth is highest priority so we check it first for best results.
+  if (curTime - lastMouth > periodMouth) {
+    lastMouth += periodMouth;
+    updateMouth();
+  }
+
+  //next see if it's time to update neck control
+  if (curTime - lastNeck > periodNeck) {
+    lastNeck += periodNeck;
+    updateNeck();
+  }
+
+  //lastly we'll look at serial input and see if we're getting any manual commands to run
+  if (Serial.available() > 0) {
+    //get command character
+    int c = Serial.read();
+
+    switch (c) {
+      case 'm': //disable auto mouth (audio control)
+        autoMouth = false;
+        break;
+
+      case 'M': //enable auto mouth
+        autoMouth = true;
+        break;
+
+      case 'n': //disable random neck (gazing)
+        autoNeck = false;
+        break;
+
+      case 'N': //enable random neck
+        autoNeck = true;
+        break;
+
+      case 'o': //mouth open halfway
+        curMouth = (MOUTH_CLOSED + MOUTH_OPEN) / 2;
+        lastManualMouth = curTime;
+        break;
+
+      case 'O': //mouth fully open
+        curMouth = MOUTH_OPEN;
+        lastManualMouth = curTime;
+        break;
+
+      case 'c': //close mouth
+        curMouth = MOUTH_CLOSED;
+        lastManualMouth = curTime;
+        break;
+
+      case 's': //set neck turn speed
+        neckSpeed = Serial.parseInt() / 100.0;
+        break;
+
+      case ',': //turn neck left at current speed
+        neckTarget = NECK_LEFT;
+        lastManualNeck = curTime;
+        break;
+
+      case '.': //turn nect right at current speed
+        neckTarget = NECK_RIGHT;
+        lastManualNeck = curTime;
+        break;
+
+      case '/': //stop manual neck turning
+        neckTarget = neckPos;
+        lastManualNeck = curTime;
+        break;
+
+      case 'f': //neck straight ahead
+        neckTarget = NECK_CENTER;
+        lastManualNeck = curTime;
+        break;
+
+      case '!': // set mouth to specified angle
+        curMouth = Serial.parseInt();
+        lastManualMouth = curTime;
+        break;
+
+      case '@': // set neck to specified angle
+        neckTarget = Serial.parseInt();
+        neckPos = neckTarget;
+        lastManualNeck = curTime;
+        break;
+    }
+  }
+} 
